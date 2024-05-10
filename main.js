e612c3b0a7f7c1a8903c37a70dc6c86ad59a2d9b
liwgar <wilenri341@hotmail.com>
2024-05-10 10:33:37 -0500

    main.js file created

---
+++ main.js
@@ -0,0 +1,26 @@
+const menuIcon = document.querySelector("#menu_Icon");
+const navBar = document.querySelector("#nav_Bar");
+const menuCloseIcon = document.querySelector("#closeMenu_Icon");
+
+const arrowDown = document.querySelector("#arrow_Down");
+const subMenuContent = document.querySelector("#subMenu_Content");
+
+menuIcon.addEventListener('click', () => {
+  if(navBar.style.display = 'block') {
+    menuCloseIcon.addEventListener('click', () => {
+      navBar.style.display = 'none';
+      subMenuContent.style.display= 'none';
+      if (subMenuContent.style.display = 'none') {
+        arrowDown.style.display = 'block'
+      }
+    });
+  }
+});
+
+arrowDown.addEventListener('click', () => {
+  subMenuContent.style.display = 'block';
+  arrowDown.style.display = 'none'
+  
+  
+});
+
