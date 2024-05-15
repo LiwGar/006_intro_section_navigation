c28caa56f328d758d500a3a0305a5f1d59115ae8
liwgar <wilenri341@hotmail.com>
2024-05-15 13:20:09 -0500

    Added functionality to display Features and Company menus on click and visual styles for screens equal to or larger than 1280px

--- main.js
+++ main.js
@@ -2,24 +2,53 @@ const menuIcon = document.querySelector("#menu_Icon");
 const navBar = document.querySelector("#nav_Bar");
 const menuCloseIcon = document.querySelector("#closeMenu_Icon");
 
-const arrowDown = document.querySelector("#arrow_Down");
-const subMenuContent = document.querySelector("#subMenu_Content");
+const featuresButton = document.querySelector("#features_Button");
+const companyButton = document.querySelector("#company_Button");
 
+const featuresArrowDown = document.querySelector("#features_arrowDown");
+const companyArrowDown = document.querySelector("#company_arrowDown");
+
+const featuresArrowUp = document.querySelector("#features_arrowUp");
+const companyArrowUp = document.querySelector("#company_arrowUp")
+
+const subMenuFeatures = document.querySelector("#subMenu_Features");
+const subMenuCompany = document.querySelector("#subMenu_Company");
+ 
 menuIcon.addEventListener('click', () => {
   if(navBar.style.display = 'block') {
+
     menuCloseIcon.addEventListener('click', () => {
       navBar.style.display = 'none';
-      subMenuContent.style.display= 'none';
-      if (subMenuContent.style.display = 'none') {
-        arrowDown.style.display = 'block'
-      }
+      subMenuFeatures.style.display = 'none';
+      subMenuCompany.style.display = 'none';
     });
   }
 });
 
-arrowDown.addEventListener('click', () => {
-  subMenuContent.style.display = 'block';
-  arrowDown.style.display = 'none';
+featuresButton.addEventListener('click', () => {
   
+  if (subMenuFeatures.style.display === 'block') {
+    subMenuFeatures.style.display = 'none';
+    featuresArrowDown.style.display = 'block';
+    featuresArrowUp.style.display = 'none';
+  } else {
+    subMenuFeatures.style.display = 'block';
+    featuresArrowDown.style.display = 'none';
+    featuresArrowUp.style.display = 'block';
+  }
 });
+
+companyButton.addEventListener('click', () => {
+  
+  if (subMenuCompany.style.display === 'block') {
+    subMenuCompany.style.display = 'none';
+    companyArrowDown.style.display = 'block';
+    companyArrowUp.style.display = 'none';
+  } else {
+    subMenuCompany.style.display = 'block';
+    companyArrowDown.style.display = 'none';
+    companyArrowUp.style.display = 'block';
+  }
+});
+
 
